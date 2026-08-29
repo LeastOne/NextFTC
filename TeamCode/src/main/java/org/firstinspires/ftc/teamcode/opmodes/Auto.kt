297645cd46b7af16a9fec83f62bbe626b0edd821
3drdProgramming <programming@3droboticsduluth.com>
2026-08-29 06:30:08 -0500

    Add Decode Quanomous autonomous strategy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.kt
@@ -1,7 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmodes
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import org.firstinspires.ftc.teamcode.subsystems.Auto.execute
 
 @Autonomous
 @Suppress("unused")
-class Auto : OpMode()
+class Auto : OpMode() {
+    override fun onStartButtonPressed() {
+        execute().schedule()
+    }
+}
